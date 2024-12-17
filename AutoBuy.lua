04c35068a9a0034cdfd2df18b2af141e8fef9550
rdx222f <166171248+rdx222f@users.noreply.github.com>
2024-12-17 10:28:28 -0500

    Update AutoBuy.lua

--- AutoBuy.lua
+++ AutoBuy.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://access.api-x.site/Rdx222"))()("/Universal/AutoBuy/-vPWNI5u3u6-vdzQRjCnxi-cCh5VkNeWD-6j2zVFOrls")
+loadstring(game:HttpGet("https://access.api-x.site/Rdx222"))()("/Universal/AutoBuy/-dlB10Ve6HH-HU4MjxwxvM-zY0umNeFUq-HezsdjB2bB-8pDHIhp8WP-dR4lS04Aej")
